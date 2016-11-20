03d0b12256256257ff704549667a5ccf33df7978
Alexander Van Le <alexandervanleprivat@gmail.com>
2016-11-20 15:41:49 +0100

    okay

---
+++ app.js
@@ -0,0 +1,6 @@
+var audioElement = document.createElement('audio');
+audioElement.setAttribute('src', 'sadfrog.mp3');
+audioElement.setAttribute('autoplay', 'autoplay');
+//audioElement.load()
+$.get();
+audioElement.play();
